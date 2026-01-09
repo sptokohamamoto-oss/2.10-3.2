ee6bd74d051e05c4a9962cb29829c70dba1c5541
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-09 13:03:50 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    class Program 
+{
+    static void Main(string[] args) 
+    {
+        double price = 123.45;
+        Console.WriteLine("価格: " + price.ToString("# .##"));
+    
+    
+    
+    
+    }
+}
